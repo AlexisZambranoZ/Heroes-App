d444de6e263032a39c3e8c66dff5518fa4dc8dce
Alexis <zambra873@gmail.com>
2023-06-15 19:29:25 -0500

    s

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { enviroments } from 'src/environments/enviroments';
 import { User } from '../interfaces/user.interface';
-import { Observable } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 
@@ -15,18 +15,37 @@ export class AuthService {
 
     get currentUser(): User | undefined {
         if (!this.user) return undefined
-        return structuredClone ( this.user)
+        return structuredClone(this.user)
 
     }
 
-    /* login(mail: string, password: string ):Observable<User>{
-        this.httpClient.get<User>( `${this.baseUrl}/users/1` )
+    login(mail: string, password: string): Observable<User> {
+        return this.httpClient.get<User>(` ${this.baseUrl}/users/1 `)
+            .pipe(
+                tap(user => this.user = user),
+                tap(user => localStorage.setItem('token', 'asaSDSAasaSAWsadas2165'))
+            )
+    }
+
+    checkAuthentication(): Observable<boolean> {
+        if(!localStorage.getItem('token')) return of (false)
+     
+        const token = localStorage.getItem('token')
+        
+        
+        return this.httpClient.get<User>(`${this.baseUrl}/users/1`)
         .pipe(
-            tap(user=> {
-
-            })
+            tap(user => this.user = user),
+            map(user => !!user),
+            catchError(err => of(false))
         )
-    } */
+    
+    }
 
-
+    logout(){
+        this.user = undefined
+        localStorage.clear()
+    }
 }
+
+
--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/auth/interfaces/user.interface';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-layout-page',
@@ -7,12 +10,22 @@ import { Component } from '@angular/core';
   ]
 })
 export class LayoutPageComponent {
-  
+
+  constructor(private authService: AuthService,
+    private router: Router) { }
+
   public sidebarItems = [
-    { label:'Listado', icon:'label',url:'./list'},
-    { label:'Añadir', icon:'add',url:'./new-hero'},
-    { label:'buscar', icon:'search',url:'./search'}
+    { label: 'Listado', icon: 'label', url: './list' },
+    { label: 'Añadir', icon: 'add', url: './new-hero' },
+    { label: 'buscar', icon: 'search', url: './search' }
   ]
 
+  onLogout() {
+    this.authService.logout()
+    this.router.navigate(['/auth/login'])
+  }
+  get user(): User | undefined {
+    return this.authService.currentUser
+  }
 
 }
