eead05fb7b600ca98dd23220357fef78b2fe07f9
Alexis Zambrano <zambra873@gmail.com>
2023-06-19 12:53:44 -0600

    a

--- src/app/auth/guards/public.guard.ts
+++ src/app/auth/guards/public.guard.ts
@@ -1,44 +1,43 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanMatch, Route, UrlSegment, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Observable, tap } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, CanMatch, Route, Router, RouterStateSnapshot, UrlSegment } from '@angular/router';
+import { Observable, map, tap } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
-@Injectable({ providedIn: 'root' })
-
-export class AuthGuard implements CanMatch, CanActivate {
-
+@Injectable({providedIn: 'root'})
+export class PublicGuard implements CanMatch, CanActivate {
     constructor(
         private authService: AuthService,
-        private router: Router) { }
+        private router: Router
+    ) { }
 
-    private checkAtuhStatus(): boolean | Observable<boolean> {
+    private checkAtuhStatusPublic(): boolean | Observable<boolean> {
         return this.authService.checkAuthentication()
             .pipe(
                 tap(isAuthenticate => {
                     console.log('Atuhenticated', isAuthenticate);
                 }),
                 tap(isAuthenticate => {
-                    if (!isAuthenticate) {
-                        this.router.navigate(['./auth/login'])
+                    if (isAuthenticate) {
+                        this.router.navigate(['./'])
                     }
-                })
+                }),
+                map(isAuthenticated => !isAuthenticated)
             )
     }
-
+    
     canMatch(route: Route, segments: UrlSegment[]): boolean | Observable<boolean> {
         /*   console.log(  'can match');
           console.log(  {route , segments });
           return true */
-        return this.checkAtuhStatus()
+        return this.checkAtuhStatusPublic()
     }
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
         /*     console.log('Can activate');
             console.log({route , state});
           
             return true */
-        return this.checkAtuhStatus()
+        return this.checkAtuhStatusPublic()
     }
-
 
 
 }
